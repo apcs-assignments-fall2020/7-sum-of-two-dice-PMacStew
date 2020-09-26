1524e263181ad643203b07de9b5dcc6c57f1f1c3
PMacStew <23pmcclure@athenian.org>
2020-09-26 23:49:05 +0000

    complete

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -5,20 +5,30 @@ public class MyMain {
     // Rolls a single die
     // Returns a random integer between 1 and 6
     public static int rollDie() {
-        // REPLACE YOUR CODE HERE
-        return -1;
+        return (int) (Math.random() * 6) + 1;
     }
 
     // Rolls two dice n times
     // This method returns an int array of size 11, where each element is
     // the number of times a certain sum of two dice was rolled
     public static int[] sumOfTwoDice(int n) {
-        // REPLACE YOUR CODE HERE
-        return null;
+        int random = 0;
+        int[] rollCount = new int[11];
+        for (int i = 0; i < n; i++) {
+            random = ((int) (Math.random() * 6) + 1) + ((int) (Math.random() * 6) + 1);
+            rollCount[random - 2]++;
+        }
+        return rollCount;
     }
 
 
     public static void main(String[] args) {
-        // YOUR CODE HERE
+        Scanner scan = new Scanner(System.in);
+        System.out.print("How many pairs of dice would you like to roll: ");
+        int amountRoll = scan.nextInt();
+        int[] percentageRoll = sumOfTwoDice(amountRoll);
+        for (int i = 0; i < 11; i++) {
+            System.out.println((((double) percentageRoll[i] / (double) amountRoll) * 100) + "% of the values are " + (i + 2) + ".");
+        }
     }
 }
